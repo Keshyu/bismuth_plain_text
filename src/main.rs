bc7ea25df808e3e504cd13998b490a036a0ee96e
Анастасия Королева <anastasiakoroleva@MacBook-Air-Anastasia-2.local>
2024-01-18 23:01:10 +0700

    Tree sitter integration

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,28 @@
-mod parser;
-
 use anyhow::{Context, Result};
-use dbg_pls::pretty as fmt_pretty;
+use dbg_pls::DebugPls;
 use std::{env, fs};
+use tree_sitter::{Parser, Tree};
 
 fn main() -> Result<()> {
     let file_name = env::args().nth(1).context("file name not provided")?;
     let source = fs::read_to_string(&file_name)?;
-    let tokens = parser::lex(&source.chars().collect::<Box<_>>())?;
-    let expression = parser::parse(&tokens)?;
+    let expression = parse(&source)?;
 
-    println!("{}", fmt_pretty(&expression));
+    println!("{:?}", &expression);
 
     Ok(())
+}
+
+fn parse(source: impl AsRef<[u8]>) -> Result<Tree> {
+    let mut parser = Parser::new();
+    parser
+        .set_language(tree_sitter_bismuth::language())
+        .context("setting Bismuth as the parsing language")?;
+    Ok(parser.parse(source, None).unwrap())
+}
+
+#[derive(DebugPls)]
+pub enum Expression {
+    Group(Box<[Expression]>),
+    Name(Box<str>),
 }
